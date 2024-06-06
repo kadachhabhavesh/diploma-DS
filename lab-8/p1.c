2d88587583f1c05e9e66e4dc057bde4b4bceadd6
Bhavesh Kadachha <151392003+kadachhabhavesh@users.noreply.github.com>
2024-06-06 22:56:54 +0530

    diploma sem-3 lab-8

---
+++ lab-8/p1.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+struct Node{
+	int info;
+	struct Node* link;
+};
+struct Node* createNode(int data){
+	struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+	newNode->info = data;
+	newNode->link = NULL;
+	return newNode;
+}
+
+void displayNode(struct Node* node){
+	printf("data : %d",node->info);
+}
+void main(){
+	int data;
+	printf("Enter node data : ");
+	scanf("%d",&data);
+	struct Node* newNode = createNode(data);
+	displayNode(newNode);
+}
